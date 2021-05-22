421ab87220b17b6d5cd95374e8f82badeeffb0ae
Frank Eguzoikpe <chukwudieguzoikpe@gmail.com>
2021-05-22 22:25:58 +0200

    InvalidLogin

--- src/test/java/Pages/ForgotPasswordPage.java
+++ src/test/java/Pages/ForgotPasswordPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
-public class ForgotPasswordPage {
+public class gitForgotPasswordPage {
 
     @FindBy(how = How.LINK_TEXT, using = "Forgot your password?")
     public static WebElement ForgotPasswordLink;
